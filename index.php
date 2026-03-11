584b0ce5dbe1789884f7dc4a8853fdcaa13ac353
RayanAmarOuali <9021944@student.zadkine.nl>
2026-03-11 07:55:31 +0100

    Homepage

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Escape Room</title>
-  <link rel="stylesheet" href="./css/style.css">
+  <title>Homepage</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
+<body class="bodyin">
+  
+<div class="divin">
+<h1 class="h1in">Welkom in de Horror Dierentuin… </h1>
 
-<body>
+<p class="pin">Je bent opgesloten in een verlaten dierentuin waar iets<br>
+vreselijk mis is gegaan. De dieren zijn niet meer <br>
+normaal.. ze zijn veranderd in gevaarlijke monsters die in<br>
+het donker rondzwerven.</p>
 
-  <h1>Welkom</h1>
-  <p>De button hieronder is alleen voor testdoeleinden bij het opstarten van het project. Deze pagina is bedoeld voor de
-    uitleg van jullie escape room.</p>
-  <p>Via deze pagina gaat een team naar de aanmeldpagina</p>
+<p class="pin">Je hebt maar een beperkte tijd om te onstappen. Los<br>
+puzzels op, vind verborgen aanwijzingen en open<br>
+gesloten ruimtes voordat de timer afloopt.</p>
 
+<p class="pin">Maar wees snel.. want zodra de tijd op is, worden de<br>
+kooien geopend en beginnen de dieren op jacht.</p>
 
-  <button><a href="./rooms/room_1.php">Klik hier voor een demonstratie van kamer
-      1</a></button>
+<p class="pin">Kun jij levend onstsnappen uit de horror dierentuin?<br>
+</div>
 
+<button class="btnin">Play</button>
+  
 </body>
-
 </html>
