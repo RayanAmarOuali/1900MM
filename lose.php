7f4c7d077c8d3340ee1399229080a0cc0370224f
RayanAmarOuali <9021944@student.zadkine.nl>
2026-03-11 10:26:11 +0100

    verlies scherm

---
+++ lose.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game over</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="bodyls">
+
+<h1 class="h1ls">U are dead!</h1>
+
+<p class="pls">You ran out of time and failed the game</p>
+
+<button class="btnls">Try Again</button> 
+
+
+
+    
+</body>
+</html>
